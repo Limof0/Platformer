83bde82627def766ec3d8b4e80541144bb5efc5e
Petek <limof2001@gmail.com>
2025-12-03 20:29:46 +0800

    Уровни игры
    Добавляю объекты и юниты для создания уровней

---
+++ levels.py
@@ -0,0 +1,7 @@
+# Структура уровня:
+# {
+#     "player_start": (x, y),
+#     "goal": (x, y),
+#     "platforms": [(x, y, width, height, type, move_range), ...],
+#     "enemies": [(x, y, patrol_range), ...]
+# }

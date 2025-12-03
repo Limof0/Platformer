74b40fe2500ca9949705e82003c53fc76348b924
Petek <limof2001@gmail.com>
2025-12-03 19:49:29 +0800

    Update game.py
    Добавил гравитацию и движение персонажа

--- game.py
+++ game.py
@@ -16,4 +16,41 @@ class Player:
         self.color = (0, 120, 255)
         self.lives = 3
         self.invincible = 0
-        self.direction = 1  # 1 - вправо, -1 - влево
+
+        self.direction = 1 # 1 - вправо, -1 - влево
+
+    
+    def update(self, platforms, enemies):
+        # Гравитация
+        self.vel_y += 0.8
+
+        # Ограничение скорости
+        self.vel_x = max(-5, min(5, self.vel_x))
+        self.vel_y = max(-20, min(20, self.vel_y))
+
+        # Движение по X
+        self.x += self.vel_x
+
+        # Проверка коллизий по X
+        for platform in platforms:
+            if self.check_collision(platform):
+                if self.vel_x > 0:
+                    self.x = platform.x - self.width
+                elif self.vel_x < 0:
+                    self.x = platform.x + platform.width
+                self.vel_x = 0
+
+        # Движение по Y
+        self.y += self.vel_y
+        self.on_ground = False
+
+        # Проверка коллизий по Y
+        for platform in platforms:
+            if self.check_collision(platform):
+                if self.vel_y > 0:
+                    self.y = platform.y - self.height
+                    self.vel_y = 0
+                    self.on_ground = True
+                elif self.vel_y < 0:
+                    self.y = platform.y + platform.height
+                    self.vel_y = 0

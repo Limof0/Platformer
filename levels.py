52bd2092bdf680516cc6443263e4101dfecae1eb
Petek <limof2001@gmail.com>
2025-12-16 23:39:04 +0800

    Update levels.py

--- levels.py
+++ levels.py
@@ -2,8 +2,8 @@
 # {
 #     "player_start": (x, y),
 #     "goal": (x, y),
-#     "platforms": [(x, y, width, height, type, move_range), ...],
-#     "enemies": [(x, y, patrol_range), ...]
+#     "platforms": [(x, y, width, height, type, move_range), ],
+#     "enemies": [(x, y, patrol_range), ]
 # }
 
 levels = [
@@ -158,5 +158,6 @@ levels = [
     },
 
  
+
 
 
